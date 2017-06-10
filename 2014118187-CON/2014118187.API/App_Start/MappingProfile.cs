9ff2024e7433248456b0cb5da370b3b67205350a
Rodrigo <rhuarac@gmail.com>
2017-06-10 17:33:49 -0500

    PC FINAL

---
+++ 2014118187-CON/2014118187.API/App_Start/MappingProfile.cs
@@ -0,0 +1,45 @@
+﻿using _2014118187_ENT.Entities;
+using _2014118187_ENT.EntitiesDTO;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _2014118187.API.App_Start
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile(){
+
+            CreateMap<ATM, ATMDTO>();
+            CreateMap<ATMDTO, ATM>();
+
+            CreateMap<BasedeDatos, BasedeDatosDTO>();
+            CreateMap<BasedeDatosDTO, BasedeDatos>();
+
+            CreateMap<Cuenta, CuentaDTO>();
+            CreateMap<CuentaDTO, Cuenta>();
+
+            CreateMap<DispensadorEfectivo, DispensadorEfectivoDTO>();
+            CreateMap<DispensadorEfectivoDTO, DispensadorEfectivo>();
+
+            CreateMap<Pantalla, PantallaDTO>();
+            CreateMap<PantallaDTO, Pantalla>();
+
+            CreateMap<RanuraDeposito, RanuraDepositoDTO>();
+            CreateMap<RanuraDepositoDTO, RanuraDeposito>();
+
+            CreateMap<Retiro, RetiroDTO>();
+            CreateMap<RetiroDTO, Retiro>();
+
+            CreateMap<Teclado, TecladoDTO>();
+            CreateMap<TecladoDTO, Teclado>();
+
+
+
+
+
+        }
+    }
+}
---
+++ 2014118187-CON/2014118187.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _2014118187.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
